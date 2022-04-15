f7c456eee85c668eb44f0ccf80b514d1a35623f9
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-04-15 15:04:33 -0400

    Implement dropdown navigation

--- js/index.js
+++ js/index.js
@@ -1,13 +1,32 @@
 const openBtn = document.getElementById("open-menu");
 const closeBtn = document.getElementById("close-menu");
 const navigation = document.getElementById("nav-wrapper");
+const features = document.getElementById("features");
+const company = document.getElementById("company");
 
-openBtn.addEventListener("click", () => {
+const openMenu = () => {
   navigation.classList.add("open");
   document.body.classList.add("lock-scroll");
-});
+};
 
-closeBtn.addEventListener("click", () => {
+const closeMenu = () => {
   navigation.classList.remove("open");
   document.body.classList.remove("lock-scroll");
+};
+
+const toggleActive = (e) => {
+  e.target.classList.toggle("active");
+};
+
+openBtn.addEventListener("click", openMenu);
+
+closeBtn.addEventListener("click", closeMenu);
+
+navigation.addEventListener("click", (e) => {
+  if (e.target.nodeName === "DIV") {
+    closeMenu();
+  }
 });
+
+features.addEventListener("click", toggleActive);
+company.addEventListener("click", toggleActive);
