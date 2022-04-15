dd783babe3caca2d9d54c4162da5d8a31eb4bc5a
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-04-15 12:14:39 -0400

    Prevent body from scrolling if menu is open

--- js/index.js
+++ js/index.js
@@ -4,8 +4,10 @@ const navigation = document.getElementById("nav-wrapper");
 
 openBtn.addEventListener("click", () => {
   navigation.classList.add("open");
+  document.body.classList.add("lock-scroll");
 });
 
 closeBtn.addEventListener("click", () => {
   navigation.classList.remove("open");
+  document.body.classList.remove("lock-scroll");
 });
